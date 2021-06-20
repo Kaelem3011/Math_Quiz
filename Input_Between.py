c234a5298536996ddb38fa9c344ecd713d13dcc3
Kaelem <51727185+Kaelem3011@users.noreply.github.com>
2021-06-21 09:55:10 +1200

    21/06/2021

--- Input_Between.py
+++ Input_Between.py
@@ -1,81 +1,72 @@
 from tkinter import *
 from functools import partial # To prevent unwanted windows
 import random
-
-
+ 
+ 
 class Start:
     def __init__(self):
+ 
+        # GUI frame
+        self.Main_GUI = Frame(padx=20, pady=10)
+        self.Main_GUI.grid()
 
-        # Heading frame
-        self.heading_frame = Frame(padx=10, pady=10)
-        self.heading_frame.grid()
-
-        # entry frame
-        self.entry_frame = Frame(padx=10, pady=10)
-        self.entry_frame.grid()
-
-
-
+        # buttons frame
+        self.buttons_frame = Frame(padx=20, pady=20)
+        self.buttons_frame.grid()
+ 
         # Quiz Heading(row 0)
-        self.math_box_label = Label(self.heading_frame, text="Math Quiz", font="Arial 19 bold")
-        self.math_box_label.grid(row=0, column=0)
+        self.math_box_label = Label(self.Main_GUI, text="Math Quiz", font="Arial 19 bold")
+        self.math_box_label.grid(row=0)
+ 
+        # entry frame
+        self.entry_frame = Frame(self.Main_GUI, padx=10, pady=10)
+        self.entry_frame.grid()
+ 
+        self.low_amount_entry = Entry(self.entry_frame, font="Arial 19 bold", width=8)
+        self.low_amount_entry.grid(row=0, column=0)
+ 
+        self.high_amount_entry = Entry(self.entry_frame, font="Arial 19 bold", width=8)
+        self.high_amount_entry.grid(row=0, column=1)
+ 
+        # Enter button
+        self.enter_button = Button(self.buttons_frame, text="Enter", command=lambda: self.in_between("'"), pady=5, padx=5)
+        self.enter_button.grid(row=3, column=0, padx=5)
 
-        # User input (row 1)
-        self.get_input_frame = Frame(self.entry_frame)
-        self.get_input_frame.grid(row=1)
-
-        # User input 2 (row 1)
-        self.get_input_frame_2 = Frame(self.entry_frame)
-        self.get_input_frame_2.grid(row=1, column=1)
-
-        self.start_amount_entry = Entry(self.get_input_frame, font="Arial 19 bold", width=8)
-        self.start_amount_entry.grid(row=0, column=0)
-
-        self.start_amount_entry = Entry(self.get_input_frame_2, font="Arial 19 bold", width=8)
-        self.start_amount_entry.grid(row=0, column=1)
-
-        self.enter_button = Button(self.get_input_frame, font="Arial 14 bold",
-                                       text="✓", command=self.error_check)
-        self.enter_button.grid(row=0, column=1)
-
-        self.enter_button_2 = Button(self.get_input_frame_2, font="Arial 14 bold",
-                                       text="✓", command=self.error_check)
-        self.enter_button.grid(row=0, column=2)
-
-                # error frame
-        self.error_frame = Frame(self.entry_frame)
-        self.error_frame.grid()
-        
+        # Quit Button
+        self.quit_button = Button(self.buttons_frame, text="Quit", command=self.to_quit, padx=5, pady=5)
+        self.quit_button.grid(row=3, column=1, padx=5)
+ 
+        # error labels
+        self.error_frame = Frame(self.Main_GUI, padx=10, pady=10)
+        self.error_frame.grid(row=2)
+ 
         self.error_1_label = Label(self.error_frame, fg="maroon",
-                                        text="error 1", font="Arial 10 bold", wrap=275,
-                                        justify=LEFT)
-        self.error_1_label.grid(row=2)
-
-        self.error_2_label = Label(self.error_frame, fg="maroon",
-                                        text="error 2", font="Arial 10 bold", wrap=275,
-                                        justify=RIGHT)
-        self.error_2_label.grid(row=2)
-
-    def error_check(self):
-        
-        input_answer = self.start_amount_entry.get()
-        has_errors = "no"
-
-        self.error_label.config(text="")
-
+                                        text="", font="Arial 9 bold", wrap=275)
+        self.error_1_label.grid(row=0, column=0)
+ 
+    # function detects if user input has min and max
+    def in_between (self, operation):
+ 
+        error_back = "red"
+        error_feedback = "no errors"
+ 
         try:
-            input_1 = int(input_answer)
-            valid_feedback = "Thanks, valid integer"
-
+            min = int(self.low_amount_entry.get())
+            max = int(self.high_amount_entry.get())
+ 
+            if min > max:
+                self.error_1_label.config(fg='maroon', text="Error: Min value > Max value")
+            
+            if max > min:
+                self.error_1_label.config(fg='green', text="Valid: Min value = {} | Max value = {}".format(min, max))
+ 
         except ValueError:
             has_errors = "yes"
-            error_feedback = "Please input an integer"
-
-        if has_errors == "yes":
-            self.error_1_label.config(text=error_feedback, fg='maroon')
-
-        else:
-            self.error_1_label.config(text=valid_feedback, fg='green')
+            self.error_1_label.config(fg='maroon', text="Enter an integer for Max and min value\nMin < Max")
+            
+    
+    def to_quit(self):
+        root.destroy()
 
 
 # main routine
@@ -84,5 +75,7 @@ if __name__ == "__main__":
     root.title("Math Quiz")
     something = Start()
     root.mainloop()
-
+ 
+ 
+ 
 
